47318c6fd243981282c1550caf440ed2ce102dd2
Developer-felix <onjombafelix1@gmail.com>
2022-03-25 12:00:31 -0700

    Prejob bootstrapping and config for mpesa

--- config/app.php
+++ config/app.php
@@ -232,4 +232,18 @@ return [
 
     ],
 
+    // 'debug_blacklist' => [
+    //     '_ENV' => [
+    //         MPESA_CONSUMER_KEY,
+    //         MPESA_CONSUMER_SECRET,
+    //         MPESA_SHORTCODE,
+    //         MPESA_STK_SHORTCODE,
+    //         MPESA_ENV,
+    //         MPESA_TEST_PHONE,
+    //         MPESA_TEST_URL,
+    //         MPESA_PASSKEY,
+    //         MPESA_B2C_PASSWORD,
+    //     ]
+    // ]
+
 ];
