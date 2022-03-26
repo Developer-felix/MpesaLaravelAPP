b175eac1d73f3fa8a8cb8a0759a71d27581537ec
Developer-felix <onjombafelix1@gmail.com>
2022-03-26 01:54:16 -0700

    getHTTP

--- app/Http/Controllers/payments/mpesa/MpesaController.php
+++ app/Http/Controllers/payments/mpesa/MpesaController.php
@@ -30,4 +30,24 @@ class MpesaController extends Controller
 
         return $response;
     }
+
+    public function makeHttp($url,$body){
+        // $url = 'https://mpesa-reflector.herokuapp.com' . $url;
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/' . $url;
+        $curl = curl_init();
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => array('Content-Type:application/json','Authorization:Bearer '. $this->getAccessToken()),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => json_encode($body)
+        );
+        curl_setopt_array($curl,$options);
+
+        $curl_response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $curl_response;
+    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+document.getElementById('getAccessToken').addEventListener('click',(event)=>{
+    event.preventDefault()
+
+    axios.post('/get-token',{})
+    .then((response)=>{
+        console.log(response.data);
+        document.getElementById('access_token').innerHTML=response.data.access_token;
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+})
