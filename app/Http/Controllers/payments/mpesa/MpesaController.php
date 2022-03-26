e331a2da71b24b4b21de7b378134b6679286cc17
Developer-felix <onjombafelix1@gmail.com>
2022-03-25 23:33:08 -0700

    Created interface

---
+++ app/Http/Controllers/payments/mpesa/MpesaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\payments\mpesa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MpesaController extends Controller
+{
+    //
+}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,8 +8,95 @@
     <title>Document</title>
 </head>
 <body>
+
     <div class="container">
-    <h1>Test</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        <div class="row mt-5">
+
+
+            <div class="col-sm-8 mix-auto">
+                <div class="card" style="padding-top: 20px;">
+                    <div class="card-header">
+                        Obtain Access Token
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Request Access Token</a>
+                      </div>
+                </div>
+
+                <div class="card" style="padding-top: 20px;">
+                    <div class="card-header">
+                        Register URL
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Request Access Token</a>
+                      </div>
+                </div>
+
+                <div class="card" style="padding-top: 20px;">
+                    <div class="card-header">
+                        Simulate Transaction
+                      </div>
+                      <div class="card-body">
+                          <form action="">
+                              @csrf
+                              <div class="form-group">
+                                  <label for="ammount">Ammount</label>
+                                  <input type="number" name="ammount" class="form-control" id="ammount">
+                              </div>
+                              <div class="form-group">
+                                  <label for="account">Account</label>
+                                  <input type="account" name="account" class="form-control" id="account">
+                               </div>
+                              <div style="padding-top: 20px;">
+                                <a href="#"  class="btn btn-primary">Simulate Payments</a>
+                              </div>
+
+                          </form>
+                      </div>
+                </div>
+
+            </div>
+          </div>
     </div>
 </body>
 </html>
